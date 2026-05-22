a2383766659de8014f5e88868ce1ee1a189349ab
Pendli Himavanth <pendlihimavanth@Pendlis-MacBook-Air.local>
2026-05-22 14:57:04 +0530

    Configure standard HTTP local server for seamless access

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  plugins: [
-    basicSsl()
-  ],
   server: {
-    https: true,
     host: true,
     port: 5173
   }
